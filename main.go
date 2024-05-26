ad66e24e34dcab0fd913c2deec83bc0ca7681f7b
AhmdFahad <ahmd66699@gmail.com>
2024-05-26 10:54:29 +0300

    learn func and condition and loop and struce and basic of go

--- main.go
+++ main.go
@@ -3,5 +3,30 @@ package main
 import "fmt"
 
 func main() {
+
+	var name = 231321
+
 	fmt.Println("Hello World!")
+	fmt.Println(r(2))
+	fmt.Printf("type of name is %T", name)
+	forLoop(10)
+}
+
+func r(h int) int {
+	x := 20
+	return x + h
+}
+
+func forLoop(count int) {
+	for i := 0; i < count; i++ {
+		fmt.Println(i)
+
+	}
+}
+
+type Person struct {
+	name   string
+	age    int
+	job    string
+	salary int
 }
